f5bfdf11e4652063f1b02f96c941142ec0c5c527
Jens John <jens.o.john@gmail.com>
2013-07-21 15:50:29 +0200

    added GPL header

--- anthy-dic-cli.c
+++ anthy-dic-cli.c
@@ -1,3 +1,21 @@
+/*
+    anthy-dic-cli - A CLI for editing the private dictionary used by Anthy
+    Copyright (C) 2013 Jens Oliver John <asterisk >swirly< 2ion >dot< de>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <values.h>
